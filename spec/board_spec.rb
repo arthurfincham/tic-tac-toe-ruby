a0b21dcff79f02dbff915bc94946880b6c824a3c
Arthur Fincham <arthurfincham@me.com>
2021-11-22 10:12:40 +0000

    spec for coordinate to index

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -12,4 +12,18 @@ describe Board do
       expect{subject.display}.to output("  A B C\n1 . . .\n2 . . .\n3 . . .\n").to_stdout
     end
   end
+  
+  describe '.get_index' do
+    it 'returns 0 for A1' do
+      expect(subject.get_index('A1')).to eq 0
+    end
+
+    it 'returns 3 for B1' do
+      expect(subject.get_index('B1')).to eq 3
+    end
+
+    it 'returns 7 for C2' do
+      expect(subject.get_index('C2')).to eq 7
+    end
+  end
 end
