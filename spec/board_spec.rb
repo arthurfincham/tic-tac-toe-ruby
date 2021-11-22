a61f8df3cf7a295033a69e5cd0403a3ffbd22a6a
Arthur Fincham <arthurfincham@me.com>
2021-11-22 10:13:11 +0000

    add spec for .turn

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -12,7 +12,7 @@ describe Board do
       expect{subject.display}.to output("  A B C\n1 . . .\n2 . . .\n3 . . .\n").to_stdout
     end
   end
-  
+
   describe '.get_index' do
     it 'returns 0 for A1' do
       expect(subject.get_index('A1')).to eq 0
@@ -24,6 +24,18 @@ describe Board do
 
     it 'returns 7 for C2' do
       expect(subject.get_index('C2')).to eq 7
+    end
+  end
+
+  describe '.turn' do
+    it 'updates the board with the turn' do
+      subject.turn('A1')
+      expect(subject.field.turn).to eq ['X', '.', '.', '.', '.', '.', '.', '.', '.']
+    end
+
+    it 'changes from X to O on second turn' do
+      subject.turn('B2')
+      expect(subject.field.turn).to eq ['X', '.', '.', '.', 'O', '.', '.', '.', '.']
     end
   end
 end
